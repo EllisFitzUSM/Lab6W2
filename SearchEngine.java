4e8ddbc57407a4650665568ca3cafd6cd841b0a8
JoveEUSM <144271740+JoveEUSM@users.noreply.github.com>
2023-11-01 14:36:48 -0400

    Subtask 3 complete

--- SearchEngine.java
+++ SearchEngine.java
@@ -2,8 +2,25 @@ package Lab6;
 
 import java.util.TreeMap;
 
+
+/*
+ * Subtask 3: You will complete SearchEngine class, the method search. When users search for
+ * something, it is expressed as a String, query, which has several words. For example, the query
+ * “All we hear is”, has four words. For each word, you have to calculate the TFxIDF (provided by
+ * your other team members) score per song. The summation of these scores is the search engine
+ * score for that song. The song with the highest score is the top result and its title should be
+ * returned.
+ * 
+ * To calculate the score for each song, find the corresponding TF values for that song. Then for
+ * each word in the query, multiply the TF and IDF values of that word
+ * 
+ * 
+ */
+
+
+
 public class SearchEngine {
-    // TreeMap of songs title and TreeMap of 
+    // TreeMap of songs title and TreeMap of words and their TFvalues for that song
     TreeMap<String, TreeMap<String, Double>> TFValues;
     
     // TreeMap of words and their IDF values
@@ -18,22 +35,28 @@ public class SearchEngine {
         IDFValues = IDF.getInverseDocumentFrequency(songTitleLyrics);        
     }
     
+    
+    /**
+     * @author Jove Emmons
+     * 
+     * @param String query The lyrics to be searched for. This can be multiple words separated by spaces (" ")
+     * 
+     * @return The String name of the song that matches the query lyrics best
+     */
     public String search(String query)
     {
-        String [] queryWords = query.split(" ");
+        String[] queryWords = query.split(" ");
         
         String resultSong = "";
         double maxScore = -1;
       
         // Iterating on each song to calculate its TF-IDF value for the query
-        for(String songTitle: songTitleLyrics.keySet())
-        {
+        for(String songTitle: songTitleLyrics.keySet()){
             // TF-IDF score for the current song
             double score = 0;
             
             // Iterating 
-            for(String queryWord: queryWords)
-            {
+            for(String queryWord: queryWords){
                 // Here you should get the corresponding TF and IDF values of that query word
                 // And then modify the code to calculate the TF-IDF value
                 
@@ -44,6 +67,15 @@ public class SearchEngine {
                 ////////////////////////////
                 //WRITE HERE
                 //////////////////////////
+            	
+            	
+            	
+            	Double TFtemp = ( TFValues.get(songTitle) ).get(queryWord);
+            	
+            	Double IDFtemp = IDFValues.get(queryWord);
+            	
+            	score += TFtemp * IDFtemp;
+            	
             }
             
             if (score>maxScore)
